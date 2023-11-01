3d0f5e43895ae8f0e16e6309ae5077d33a47107b
aefehr <fehr.allie@gmail.com>
2023-11-01 08:36:26 -0400

    Adding logout by inactivity

--- frontend/MiniGUI.py
+++ frontend/MiniGUI.py
@@ -1,47 +1,38 @@
 from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton
 from PySide6.QtGui import QFont, Qt
 from PySide6.QtCore import Qt
-from backend.cores_sqlite3 import User, Event
 from datetime import datetime
 from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import Qt, QTimer, QPoint
+from PySide6.QtGui import QGuiApplication, QCursor
 
 class MiniGUI(QWidget):
     def __init__(self, name, email,login_event, window, parent=None):
         super().__init__(parent)
         self.window = window
-
-        self.expanded = True  # Start with the window expanded
+        self.expanded = True
         self.initial_size = None
-
         self.login_event = login_event
-
+        
+        # Mouse Idle Detection setup
+        self.last_mouse_position = QPoint()
+        self.idle_timer = QTimer(self)
+        self.idle_timer.timeout.connect(self.check_mouse_idle)
+        # 5 seconds for testing
+        self.idle_timer.start(5000)
+        
         self.setStyleSheet("background-color: #FFFFFF;")
-
-        # Set window flags to hide the title bar
         self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
-
+        
         main_layout = QHBoxLayout(self)
 
         # Left side layout for user info
         left_layout = QVBoxLayout()
-
-        # Get user information from the database using email
-        #user = User.from_database_by_email(email)
-
-        #if user:
-            # User name
-            #name_label = QLabel(user.name)
-            #name_label.setFont(QFont("Arial", 16, QFont.Bold))
-            #left_layout.addWidget(name_label)
         
         # Displaying the provided name
         name_label = QLabel(name)
         name_label.setFont(QFont("Arial", 16, QFont.Bold))
         left_layout.addWidget(name_label)
-
-        # User email
-        #email_label = QLabel(user.email)
-        #left_layout.addWidget(email_label)
 
         # Remaining user info
         email_label = QLabel(email)
@@ -70,6 +61,22 @@ class MiniGUI(QWidget):
         primary_screen = QGuiApplication.primaryScreen()
         screen_geometry = primary_screen.geometry()
         self.move(screen_geometry.left(), screen_geometry.bottom() - self.height())
+    
+    def check_mouse_idle(self):
+        current_mouse_position = QCursor.pos()
+
+        if current_mouse_position == self.last_mouse_position:
+            print("Mouse is idle.")
+            # Logout after inactivity
+            self.logout()
+        else:
+            print(f"Mouse moved to ({current_mouse_position.x()}, {current_mouse_position.y()})")
+            self.last_mouse_position = current_mouse_position
+    
+    def mouseMoveEvent(self, event):
+        # If the mouse moves, reset the timer
+        self.last_mouse_position = QCursor.pos()
+        super().mouseMoveEvent(event)
 
     def logout(self):
         if self.login_event:
@@ -82,10 +89,12 @@ class MiniGUI(QWidget):
             else:
                 print("Error recording logout event.")
 
-            # Close the MiniGUI and show the main window
+            # Stop the idle timer and close the MiniGUI
+            self.idle_timer.stop()
             self.close()
             self.window.show()
 
     def toggle_size(self):
         # Minimize the window
         self.showMinimized()
+    
